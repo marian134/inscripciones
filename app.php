00346245efa81a11ab6e1753a0a6be9dbc92d08d
Mariano <mariano.d.vallejos@gmail.com>
2018-01-29 17:25:11 -0300

    Formatos App

---
+++ app.php
@@ -0,0 +1,30 @@
+<?php
+
+use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+// Use APC for autoloading to improve performance.
+// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
+// with other applications also using APC.
+$apcLoader = new ApcClassLoader('sge_kernel', $loader);
+$loader->unregister();
+$apcLoader->register(true);
+
+require_once __DIR__.'/../app/AppKernel.php';
+require_once __DIR__.'/../app/AppCache.php';
+
+
+$enableDebug = false;
+
+$kernel = new AppKernel('prod', $enableDebug);
+$kernel->loadClassCache();
+$cachedkernel = new AppCache($kernel);
+
+// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
+Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $cachedkernel->handle($request);
+$response->send();
+$cachedkernel->terminate($request, $response);
